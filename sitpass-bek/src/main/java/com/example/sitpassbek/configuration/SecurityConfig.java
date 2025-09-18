865129560609fb2d618c6526bde7bb7f608aeedf
Stefan <milutinovic.stefan2003@gmail.com>
2025-09-18 17:20:37 +0200

    add review and corss origin

--- sitpass-bek/src/main/java/com/example/sitpassbek/configuration/SecurityConfig.java
+++ sitpass-bek/src/main/java/com/example/sitpassbek/configuration/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +51,20 @@ public class SecurityConfig {
                         jwtAuthFIlter, UsernamePasswordAuthenticationFilter.class
                 );
         return httpSecurity.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // svi endpoint-i
+                        .allowedOrigins("http://localhost:4200") // Angular dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 
